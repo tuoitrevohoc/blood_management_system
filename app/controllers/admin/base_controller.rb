036c4468aad7c4340a6adf82efa80bc58f01df23
Son Tran <sontd.it@gmail.com>
2017-04-22 19:03:29 +0700

    change follow create history

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,12 @@
 class Admin::BaseController < ApplicationController
+  before_action :admin_and_limited_only!
   layout "application_admin"
+
+  protected
+  def admin_and_limited_only!
+    unless current_user.admin? || current_user.limited?
+      flash[:danger] = "Bạn không có quyền truy cập."
+      redirect_to root_path
+    end
+  end
 end
--- app/models/admin_history.rb
+++ app/models/admin_history.rb
@@ -4,4 +4,9 @@ class AdminHistory < ApplicationRecord
   belongs_to :admin, class_name: User.name, foreign_key: :admin_id
 
   validates :user, :start_time, :end_time, :admin_id, presence: true
+  validates :end_time, date: {after: :start_time}
+
+  scope :available, -> current = Time.current do
+    ransack(start_time_lteq: current, end_time_gteq: current).result
+  end
 end
