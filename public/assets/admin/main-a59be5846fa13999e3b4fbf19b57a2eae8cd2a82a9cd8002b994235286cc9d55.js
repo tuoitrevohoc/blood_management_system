4b63f6b63463adae5e311a5e7256340fd5f6b1eb
Son Tran <sontd.it@gmail.com>
2017-04-27 11:32:31 +0700

    Config and compile for deploy heroku

---
+++ public/assets/admin/main-a59be5846fa13999e3b4fbf19b57a2eae8cd2a82a9cd8002b994235286cc9d55.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+  $(".select-on-focus").on("focus", function() {
+    $(this).select();
+  });
+});
+
+function initialize_date_picker(dp_selector) {
+  return $(dp_selector).datetimepicker({
+      useCurrent: false,
+      format: "YYYY/MM/DD",
+      showClose: true,
+      showClear: true,
+      icons: {
+        close: "fa fa-check"
+      }
+    });
+}
+
+function capchaChecked() {
+  return $("#capcha_confirmation:checked").length > 0 ? true : false;
+}
+
+function selectOnFocus(selector) {
+  $(selector).on("focus", function() {
+    $(this).select();
+    return;
+  });
+}
+
+function initialize_locationpicker() {
+  $(".location-picker").locationpicker({
+    radius: DEFAULT_RADIUS,
+    inputBinding: {
+      latitudeInput: $(".lp-lat"),
+      longitudeInput: $(".lp-lon"),
+      locationNameInput: $(".lp-input")
+    },
+    enableAutocomplete: true
+  });
+}
+
+function try_current_position() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var current_position = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      };
+      $(".location-picker").locationpicker("location", current_position);
+    });
+  } else {
+    var current_position = {latitude: DEFAULT_LAT, longitude: DEFAULT_LNG};
+    $(".location-picker").locationpicker("location", current_position);
+  }
+}
+;
