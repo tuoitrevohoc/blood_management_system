d70fff2ec2a1c2a1f562956e98edf5893b91140d
Son Tran <sontd.it@gmail.com>
2017-04-28 21:38:09 +0700

    Fix map toggle

--- app/assets/javascripts/common_constants.js
+++ app/assets/javascripts/common_constants.js
@@ -1,2 +1,5 @@
 const GM_KEY = "AIzaSyA5WpyjImkemkAiHkeZQYqHEc5ybF0uIIg";
 const GM_API_GEOCODE_URL = "https://maps.googleapis.com/maps/api/geocode/json";
+const DEFAULT_RADIUS = 160;
+const DEFAULT_LAT = 21.0227731;
+const DEFAULT_LNG = 105.8018581;
