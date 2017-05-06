bc485791fa7a85417aba8cd2ce97c2471ae9e0b7
Son Tran <sontd.it@gmail.com>
2017-05-06 09:15:52 +0700

    article management

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,4 +1,11 @@
 class Admin::ArticlesController < Admin::BaseController
+  before_action :load_article, only: [:edit, :update, :destroy]
+
+  def index
+    @q = Article.ransack params[:q]
+    @articles = @q.result.page(params[:page]).per 10
+  end
+
   def new
     @article = Article.new
   end
@@ -7,10 +14,31 @@ class Admin::ArticlesController < Admin::BaseController
     @article = Article.new article_params
     if @article.save
       flash[:success] = is_public? ? "Đã đăng bài lên trang chủ." : "Đã lưu vào nháp."
-      redirect_to new_admin_article_path
+      redirect_to admin_articles_path
     else
       render :new
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update article_params
+      flash[:success] = is_public? ? "Thay đổi đã được lưu." : "Đã đóng bài viết."
+      redirect_to admin_articles_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @article.destroy
+      flash[:success] = "Đã xóa bài viết."
+    else
+      flash[:danger] = "Lỗi! không thể xóa bài viết lúc này."
+    end
+    redirect_to admin_articles_path
   end
 
   private
@@ -25,5 +53,9 @@ class Admin::ArticlesController < Admin::BaseController
 
   def make_slug
     params[:article][:title].parameterize  << "-" << SecureRandom.hex(3).upcase
+  end
+
+  def load_article
+    render_404 unless (@article = Article.find_by id: params[:id])
   end
 end
--- app/decorators/article_decorator.rb
+++ app/decorators/article_decorator.rb
@@ -11,4 +11,14 @@ class ArticleDecorator < Draper::Decorator
     original = content_parsed.present? ? content_parsed.text : strip_tags(object.content)
     original.length < limit ? original : original.first(limit) << "..."
   end
+
+  def display_status
+    lb_class = :default
+    lb_text = :private
+    if is_public?
+      lb_class = :primary
+      lb_text = :Public
+    end
+    "<span class=\"label label-#{lb_class}\">#{lb_text}</span>".html_safe
+  end
 end
