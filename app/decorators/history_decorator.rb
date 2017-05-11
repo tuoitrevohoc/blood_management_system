2ecb7d58e2cb74bb368073a8e971919bc54cdfdf
Son Tran <sontd.it@gmail.com>
2017-05-11 23:30:32 +0700

    Histories management

--- app/decorators/history_decorator.rb
+++ app/decorators/history_decorator.rb
@@ -4,4 +4,22 @@ class HistoryDecorator < Draper::Decorator
   def display_volume
     I18n.t "histories.volumes.#{volume}"
   end
+
+  def timestamp
+    I18n.l created_at, format: :full
+  end
+
+  def volume_label
+    lb_class = case volume.to_sym
+    when :ml_250
+      "info"
+    when :ml_350
+      "success"
+    when :ml_450
+      "warning"
+    else
+      "danger"
+    end
+    "<span class='label label-#{lb_class}'>#{display_volume}</span>".html_safe
+  end
 end
