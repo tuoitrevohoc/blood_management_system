21c21362c1ec7e4b11297682cf078b90a6dd0aff
Son Tran <sontd.it@gmail.com>
2017-07-10 22:42:39 +0700

    Allow edit patient info

--- app/controllers/ajax/histories_controller.rb
+++ app/controllers/ajax/histories_controller.rb
@@ -1,12 +1,23 @@
 class Ajax::HistoriesController < BaseController
-  before_action :load_history, only: :show
+  before_action :load_history, only: [:show, :update]
 
   def show
-    @history = @history.decorate if @history
+    @history = @history.decorate
+  end
+
+  def update
+    @history.update patient_params
+    @history = @history.decorate
   end
 
   private
   def load_history
-  @history = History.find_by_id params[:id]
+    @history = History.find_by_id params[:id]
+    render js: "showNotification('Không tìm thấy trang yêu cầu', 'danger');" unless @history
+  end
+
+  def patient_params
+    params.require(:history).permit :patient_name, :patient_age, :patient_description,
+      :patient_phone_number, :patient_address, :patient_pathological
   end
 end
